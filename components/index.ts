c4d0620e650244464a6804565aab5c0176bb7b7e
Eltab3e <ahmed.eltabiie@gmail.com>
2023-07-05 15:09:38 +0300

    implement search & dropdown menu

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
 import CustomButton from './CustomButton';
+import CustomFilter from './CustomFilter';
 
-export { Navbar, Hero, Footer, CustomButton };
+export {
+  Navbar,
+  Hero,
+  Footer,
+  SearchBar,
+  SearchManufacturer,
+  CustomButton,
+  CustomFilter,
+};
