0341ef5ccc609c4b63072b0d073767cc2c63a2a4
Eltab3e <ahmed.eltabiie@gmail.com>
2023-07-05 13:02:59 +0300

    add: hero section

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomButton from './CustomButton';
+
+export { Navbar, Hero, Footer, CustomButton };
